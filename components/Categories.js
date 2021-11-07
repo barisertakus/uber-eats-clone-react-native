032d7f93905ea290a22bb82bfe5a1bbe595e85b1
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-07 20:34:23 +0300

    Categories added.

---
+++ components/Categories.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+
+const items = [
+  {
+    image: require("../assets/images/shopping-bag.png"),
+    text: "Pick-up",
+  },
+  {
+    image: require("../assets/images/soft-drink.png"),
+    text: "Soft Drinks",
+  },
+  {
+    image: require("../assets/images/bread.png"),
+    text: "Bakery Items",
+  },
+  {
+    image: require("../assets/images/fast-food.png"),
+    text: "Fast Foods",
+  },
+  {
+    image: require("../assets/images/deals.png"),
+    text: "Deals",
+  },
+  {
+    image: require("../assets/images/coffee.png"),
+    text: "Coffee & Tea",
+  },
+  {
+    image: require("../assets/images/desserts.png"),
+    text: "Desserts",
+  },
+];
+
+const Categories = () => {
+  return (
+    <View style={styles.container}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {items.map((item, index) => (
+          <View style={styles.category} key={index}>
+            <Image source={item.image} style={styles.image} />
+            <Text style={styles.header}>{item.text}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default Categories;
+
+const styles = StyleSheet.create({
+  container:{
+    marginTop: 5,
+    backgroundColor: '#FFF',
+    paddingVertical: 10,
+    paddingLeft: 20
+  },
+  image: {
+    width: 50,
+    height: 40,
+    resizeMode: "contain",
+  },
+  header: {
+    fontSize: 13,
+    fontWeight: "900",
+  },
+  category: {
+    alignItems: "center",
+    marginRight: 30,
+  },
+});
--- screens/Home.js
+++ screens/Home.js
@@ -1,28 +1,30 @@
-import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import HeaderTabs from '../components/Header/HeaderTabs'
-import SearchBar from '../components/SearchBar'
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import Categories from "../components/Categories";
+import HeaderTabs from "../components/Header/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 
 const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.tabContainer}>
-      <HeaderTabs />
-      <SearchBar />
+        <HeaderTabs />
+        <SearchBar />
       </View>
+      <Categories />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     backgroundColor: "#eee",
     flex: 1,
   },
   tabContainer: {
     backgroundColor: "white",
     padding: 15,
-  }
-})
+  },
+});
