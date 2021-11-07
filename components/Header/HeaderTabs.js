ec8b2a532efeef9ae284bcab3058d3ab31920d5c
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-07 16:27:45 +0300

    The initial folder structure is created.

---
+++ components/Header/HeaderTabs.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import HeaderButton from './HeaderButton'
+
+const HeaderTabs = () => {
+  return (
+    <View>
+      <HeaderButton />
+      <HeaderButton />
+    </View>
+  )
+}
+
+export default HeaderTabs
+
+const styles = StyleSheet.create({})
---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import HeaderTabs from '../components/Header/HeaderTabs'
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <HeaderTabs />
+    </SafeAreaView>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
