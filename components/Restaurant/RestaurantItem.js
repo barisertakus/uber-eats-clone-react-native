ed0f5d85f4df3265b727a39ff6d9b9ef5ebffc73
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-10 02:15:50 +0300

    restaurantData bug fixed.

--- components/Restaurant/RestaurantItem.js
+++ components/Restaurant/RestaurantItem.js
@@ -4,13 +4,13 @@ import ResturantImage from "./ResturantImage";
 import ResturantInfo from "./ResturantInfo";
 
 const RestaurantItem = ({ restaurantData }) => {
-  const restaurants = restaurantData ? restaurantData.businesses : [];
+  const restaurants = restaurantData?.businesses || []
 
   return (
     <View style={styles.container}>
       {restaurants.map((restaurant) => (
-        <TouchableOpacity activeOpacity={0.7}>
-          <View style={styles.restaurantCard} key={restaurant.id}>
+        <TouchableOpacity activeOpacity={0.7} key={restaurant.id}>
+          <View style={styles.restaurantCard}>
             <ResturantImage image={restaurant.image_url} />
             <ResturantInfo name={restaurant.name} rating={restaurant.rating} />
           </View>
