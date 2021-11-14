1ff5d14768b0d7ad5d90c718171f1eb94988f891
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-14 20:37:32 +0300

    MenuItems added.

--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Divider } from 'react-native-elements'
 import About from '../components/restaurantDetail/About'
+import MenuItems from '../components/restaurantDetail/MenuItems'
 
 const RestaurantDetail = () => {
   return (
     <View>
       <About />
       <Divider style={styles.divider} width={1.8} />
-
+      <MenuItems />
     </View>
   )
 }
