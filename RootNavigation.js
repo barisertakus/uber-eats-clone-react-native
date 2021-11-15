3504935520e2c42b6161715decbb2a03bd3c1340
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-16 02:33:30 +0300

    redux and cartReducer added.

--- RootNavigation.js
+++ RootNavigation.js
@@ -1,24 +1,30 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
-import React from 'react'
-import Home from "./screens/Home"
-import RestaurantDetail from "./screens/RestaurantDetail"
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import Home from "./screens/Home";
+import RestaurantDetail from "./screens/RestaurantDetail";
+import { Provider } from "react-redux";
+import configureStore from "./redux/store";
+
+const store = configureStore();
 
 const RootNavigation = () => {
   const Stack = createStackNavigator();
 
   const screenOptions = {
-    headerShown: false
-  }
-  
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={screenOptions} initialRouteName="home">
-        <Stack.Screen name="home" component={Home} />
-        <Stack.Screen name="restaurantDetail" component={RestaurantDetail} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
-}
+    headerShown: false,
+  };
 
-export default RootNavigation
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={screenOptions} initialRouteName="home">
+          <Stack.Screen name="home" component={Home} />
+          <Stack.Screen name="restaurantDetail" component={RestaurantDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default RootNavigation;
