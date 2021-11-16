e9ba1d77b43d3f3f6f25b13141fcb57e5d59faea
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-16 17:24:36 +0300

    View cart button updated and totalPrice added.

--- components/restaurantDetail/ViewCart.js
+++ components/restaurantDetail/ViewCart.js
@@ -1,29 +1,43 @@
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useSelector } from "react-redux";
 
 const ViewCart = () => {
-  return (
+  const items = useSelector((state) => state.cartReducer.selectedItems.items);
+  const total = items
+    .map((item) => Number(item.price.replace("$", "")))
+    .reduce((prev, curr) => prev + curr, 0);
+
+  const totalPrice = total.toLocaleString("en", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  return total ? (
     <View style={styles.container}>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button}>
           <Text style={styles.text}>View Cart</Text>
+          <Text style={styles.textPrice}>{totalPrice}</Text>
         </TouchableOpacity>
       </View>
     </View>
+  ) : (
+    <></>
   );
 };
 
 export default ViewCart;
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
     position: "absolute",
     bottom: 30,
-    zIndex: 999
+    zIndex: 999,
   },
   buttonContainer: {
     width: "100%",
@@ -33,12 +47,19 @@ const styles = StyleSheet.create({
   text: {
     color: "white",
     fontSize: 20,
+    marginRight: 30,
+  },
+  textPrice: {
+    color: "white",
+    fontSize: 20,
   },
   button: {
     marginTop: 20,
     backgroundColor: "black",
+    flexDirection: "row",
+    justifyContent: "flex-end",
     alignItems: "center",
-    padding: 13,
+    padding: 15,
     borderRadius: 30,
     width: 300,
     position: "relative",
