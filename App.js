831ef04fad922757252801a998ee73fb5eee1e4e
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-21 03:56:55 +0300

    android timer log ignored.

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import RootNavigation from './RootNavigation';
 import { LogBox } from 'react-native';
 
-LogBox.ignoreLogs(["AsyncStorage has been extracted from react-native"]);
+LogBox.ignoreLogs(["AsyncStorage has been extracted from react-native"],
+["Setting a timer for a long perido of time"]);
 export default function App() {
   return (
     <RootNavigation />
